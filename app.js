2ccdabadf97b0e9ff8b28440ec2a778309a4a2c5
mjpwebsites <smallgames755@gmail.com>
2025-01-25 13:25:47 +0800

    Update app.js

--- app.js
+++ app.js
@@ -17,11 +17,11 @@ document.addEventListener('DOMContentLoaded', () => {
 
   // Sample portfolio items
   const portfolioItems = [
-    { category: 'wedding', image: 'https://cdn.discordapp.com/attachments/1184537320569966594/1331473327637790743/FB_IMG_1737518312554.jpg?ex=6791beaa&is=67906d2a&hm=f7709060d4ab405800bb8c9924fe93b2e1f37c08bed9a8463870da5872b5c08e&' },
-    { category: 'portrait', image: 'https://cdn.discordapp.com/attachments/1184537320569966594/1331472327262863421/FB_IMG_1737518126197.jpg?ex=6791bdbc&is=67906c3c&hm=614ccec5b45f6dbf107f73f80355d36cec25ebd7690a5745762e438ab24bbaca&' },
+    { category: 'wedding', image: 'https://ibb.co/M5L00JK' },
+    { category: 'portrait', image: 'https://ibb.co/6X0gZHK' },
     { category: 'commercial', image: 'https://source.unsplash.com/random/400x300/?commercial' },
     { category: 'wedding', image: 'https://cdn.discordapp.com/attachments/1184537320569966594/1331473889338986538/facebook_1737518464090_7287680644009942888.jpg?ex=6791bf30&is=67906db0&hm=cd109b9947f124e3d21b0c7895a6d19746062733820a45d5886a02a3f71eb969&' },
-    { category: 'portrait', image: 'https://cdn.discordapp.com/attachments/1184537320569966594/1331471681637711937/FB_IMG_1737517956440.jpg?ex=6791bd22&is=67906ba2&hm=3c3ad8ba9dbca26b9d5bb7f9e3fa0798d34fad10741024020de1938caf0acec8&' },
+    { category: 'portrait', image: 'https://ibb.co/TkmZXw1' },
     { category: 'commercial', image: 'https://source.unsplash.com/random/400x300/?business' }
   ];
 
